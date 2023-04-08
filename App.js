311fa51aefff3edd38ff932579ee4f573e572ff9
Aditya Mandley <mandleyaditya18@gmail.com>
2023-04-08 20:51:48 +0530

    poppins font added

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Poppins_400Regular as poppins400,
+  Poppins_500Medium as poppins500,
+  Poppins_600SemiBold as poppins600,
+} from '@expo-google-fonts/poppins'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ poppins400, poppins500, poppins600 })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -16,5 +29,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontFamily: 'poppins500',
   },
 })
